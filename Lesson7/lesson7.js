04ae71b9562a8464fb41b2bb2f09b717f0415f35
HoangAnhTuna <88965746+HoangAnhTuna@users.noreply.github.com>
2021-10-10 16:04:23 +0700

    ok
    ok

---
+++ Lesson7/lesson7.js
@@ -0,0 +1,48 @@
+//let a = prompt("TELL ME, WHAT IS A")
+//
+//if(a>50){
+//    alert("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+//}else{
+//    alert("ok")
+//}
+
+//let a = prompt("What is your age")
+//a = Number(a)
+
+//if(a>=30){
+//    alert("HAHA U OLD")
+//}else if(a<18){
+//    alert("hello, kid")
+//}else {
+//    alert("cool")
+//}
+//if(a>100){
+//    alert("wait, what the...")
+//}
+
+// function afternoonActivity(day) {
+//   console.log("day " + day);
+//   console.log("eat dinner");
+//   console.log("do homework ");
+//   console.log("[REDACTED]");
+//   console.log("World Cup :v");
+// }
+// afternoonActivity(2);
+// afternoonActivity(3);
+// afternoonActivity(4);
+
+function Tong(a,b) {
+    console.log(`${a} + ${b} = ${a + b}`)
+}
+
+function Hieu(a,b) {
+    console.log(`Ta có ${a} - ${b} = ${a-b}`)
+}
+function tich(a,b) {
+    console.log(`Ta có ${a} x ${b} = ${a*b}`)
+}
+function Thuong(a,b) {
+    if(b==0){
+        console.log("Không hợp lệ")
+    }else{console.log(`Ta có ${a} / ${b} = ${a/b}`)}
+}
---
+++ Lesson7/lesson7_dom/dom.js
@@ -0,0 +1,13 @@
+let light = document.getElementById("a")    
+
+console.dir(light)
+
+function on(){
+    light.src="https://www.w3schools.com/js/pic_bulbon.gif"
+}
+
+function off(){
+    light.src="https://www.w3schools.com/js/pic_bulboff.gif"
+}
+
+
